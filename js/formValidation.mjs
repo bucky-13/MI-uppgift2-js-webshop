7d1f88082fe0886b5976c0e68acf8f1abc0ff4a4
Simon Stenström <simon.stenstrom.013@gmail.com>
2023-11-27 17:31:53 +0100

    some setup on form validation function

--- js/formValidation.mjs
+++ js/formValidation.mjs
@@ -1,5 +1,29 @@
-function formValidation() {
-  console.log('test');
+const inputs = [
+  document.querySelector('#inputFirstName'),
+  document.querySelector('#inputLastName'),
+  document.querySelector('#inputAddress'),
+  document.querySelector('#inputPostNum'),
+  document.querySelector('#inputCity'),
+  document.querySelector('#inputPhone'),
+  document.querySelector('#inputEmail'),
+  document.querySelector('#inputPersonalNum'),
+  document.querySelector('#inputCardNum'),
+  document.querySelector('#inputDD'),
+  document.querySelector('#inputYY'),
+  document.querySelector('#inputCVC'),
+];
+
+const submitOrderBtn = document.querySelector('#submitOrderBtn');
+
+function formEventListener() {
+  inputs.forEach((input) => {
+    input.addEventListener('focusout', formValidation);
+    input.addEventListener('change', formValidation);
+  });
 }
 
-export default formValidation;
+function formValidation(e) {
+  console.log(e.target.id);
+}
+
+export default formEventListener;
